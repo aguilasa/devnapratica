f0f0fd50169633c882dd6474bc4c1f5c84ae6732
Ingmar Aguiar <ingmar.aguiar@senior.com.br>
2019-06-21 17:10:48 -0300

    Nova limpeza

--- back/java/impl/src/main/java/br/com/senior/furb/basico/cliente/ClienteRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package br.com.senior.furb.basico.cliente;
-
-public interface ClienteRepositoryCustom {
-	
-	boolean alteraStatusCreditoHabilitado(String id);
-
-}
--- back/java/impl/src/main/java/br/com/senior/furb/basico/repositories/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.senior.furb.basico.repositories;
-
-import org.springframework.stereotype.Repository;
-
-import br.com.senior.furb.basico.ClienteBaseRepository;
-@Repository
-public interface ClienteRepository extends ClienteBaseRepository {
-
-}
